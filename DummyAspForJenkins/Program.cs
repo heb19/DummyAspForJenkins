0508d15ee080600e9e4dcff8a759be79f0685c92
Heiko Berger <heikoberger@karldischinger.de>
2020-03-06 10:58:49 +0100

    dummy

---
+++ DummyAspForJenkins/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DummyAspForJenkins
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
